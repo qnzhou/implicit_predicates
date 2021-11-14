7aa153c931fbd6c9e8dc6c7d7bb3cc72f6107eb1
Qingnan Zhou <qzhou@adobe.com>
2021-11-13 20:55:08 -0500

    Update abseil.

--- cmake/absl.cmake
+++ cmake/absl.cmake
@@ -6,10 +6,11 @@ include(FetchContent)
 FetchContent_Declare(
     absl
     GIT_REPOSITORY https://github.com/abseil/abseil-cpp.git
-    GIT_TAG 20210324.2
+    GIT_TAG 20211102.0
     )
 
 set(OLD_CXX_STANDARD ${CMAKE_CXX_STANDARD})
 set(CMAKE_CXX_STANDARD 17)
+set(ABSL_PROPAGATE_CXX_STD On CACHE BOOL "")
 FetchContent_MakeAvailable(absl)
 set(CMAKE_CXX_STANDARD ${OLD_CXX_STANDARD})
